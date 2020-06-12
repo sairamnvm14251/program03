ba7be317d10402bc1adaac0bf3db4ff5f50da55f
unknown <sairam14251@gmail.com>
2020-06-12 15:53:28 +0530

    modification in rating controller

--- JavaSpringandRest/src/main/java/com/example/demo/controller/Rating.java
+++ JavaSpringandRest/src/main/java/com/example/demo/controller/Rating.java
@@ -1,13 +1,10 @@
 package com.example.demo.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.JavaSpringandRestApplication;
 import com.example.demo.entity.Movie;
 import com.example.demo.hsqldbdao.CustomerRatingRepository;
 
@@ -22,7 +19,7 @@ public class Rating {
 	
 	@GetMapping(value = "path/api/rest/customer/{id}/rate/{rating}", produces = "application/json")
 	
-	public Optional<Movie> customerRating(@PathVariable("id") Integer id, @PathVariable("rating") Integer rating) {
+	public Iterable<Movie> customerRating(@PathVariable("id") Integer id, @PathVariable("rating") Integer rating) {
 		System.out.println(id + "," + rating);
 		Movie m=new Movie();
 		m.setAverageRating(id);
@@ -32,7 +29,7 @@ public class Rating {
 		m.setCustomerAverageRating(2.32);		
 		repo.save(m);	
 		
-	 Optional<Movie> optionalEntity = repo.findById(id);
+	 Iterable<Movie> optionalEntity = repo.findAll();
 	 
 		System.out.println("movie rating"+optionalEntity);
 		return optionalEntity;
