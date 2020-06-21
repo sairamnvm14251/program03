da772e104124418fe4b79ab8528ba693f587531b
Sairam NVM <sairam14251@gmail.com>
2020-06-21 09:14:07 +0530

    added the comment to improve the redability of the method

--- JavaSpringandRest/src/main/java/com/example/demo/controller/Rating.java
+++ JavaSpringandRest/src/main/java/com/example/demo/controller/Rating.java
@@ -18,7 +18,7 @@ public class Rating {
 
 	
 	@GetMapping(value = "path/api/rest/customer/{id}/rate/{rating}", produces = "application/json")
-	
+	//this method is binded to get request
 	public Iterable<Movie> customerRating(@PathVariable("id") Integer id, @PathVariable("rating") Integer rating) {
 		System.out.println(id + "," + rating);
 		Movie m=new Movie();
