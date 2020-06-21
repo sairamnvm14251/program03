3cc7eaa9236eb92555166b817be6a7cbb3d44fa3
Sairam NVM <sairam14251@gmail.com>
2020-06-21 09:43:52 +0530

    added from local

--- JavaSpringandRest/src/main/java/com/example/demo/controller/Rating.java
+++ JavaSpringandRest/src/main/java/com/example/demo/controller/Rating.java
@@ -18,6 +18,8 @@ public class Rating {
 
 	
 	@GetMapping(value = "path/api/rest/customer/{id}/rate/{rating}", produces = "application/json")
+	
+	//added from local
 	public Iterable<Movie> customerRating(@PathVariable("id") Integer id, @PathVariable("rating") Integer rating) {
 		System.out.println(id + "," + rating);
 		Movie m=new Movie();
