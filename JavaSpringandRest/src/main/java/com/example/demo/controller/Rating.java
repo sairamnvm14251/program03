2bb83f67d7fd84b5350e554346dc5c3d7537b7d0
sairam <65670329+sairamnvm14251@users.noreply.github.com>
2020-06-21 09:38:06 +0530

    added form central repo

--- JavaSpringandRest/src/main/java/com/example/demo/controller/Rating.java
+++ JavaSpringandRest/src/main/java/com/example/demo/controller/Rating.java
@@ -18,6 +18,7 @@ public class Rating {
 
 	
 	@GetMapping(value = "path/api/rest/customer/{id}/rate/{rating}", produces = "application/json")
+	//added comment through central repo
 	public Iterable<Movie> customerRating(@PathVariable("id") Integer id, @PathVariable("rating") Integer rating) {
 		System.out.println(id + "," + rating);
 		Movie m=new Movie();
